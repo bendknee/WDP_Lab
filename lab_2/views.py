ede33d34119e0ae06f0f86e74f850ab3dd98523f
Benny William Pardede <bpardedewilliam@gmail.com>
2017-09-12 14:40:24 +0700

    Fixed test problem

--- lab_2/views.py
+++ lab_2/views.py
@@ -3,7 +3,7 @@ from lab_1.views import mhs_name, birth_date
 
 #TODO Implement
 #Create a content paragraph for your landing page:
-landing_page_content = "Feel free to poke around and probably leave some of your critiques. While you're here, don't forgot to follow all my social media accounts. Thank you. "
+landing_page_content = "Feel free to poke around and probably leave some of your critiques. While you are here, do not forgot to follow all my social media accounts. Thank you. "
 
 def index(request):
     response = {'name': mhs_name, 'content': landing_page_content}
