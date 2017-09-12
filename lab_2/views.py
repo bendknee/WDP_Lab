7b57e459b4f5f8367c7df215c3e6fb5054b6f9d5
Benny William Pardede <bpardedewilliam@gmail.com>
2017-09-12 13:28:22 +0700

    Implemented Landing Page Text & URL

--- lab_2/views.py
+++ lab_2/views.py
@@ -3,7 +3,7 @@ from lab_1.views import mhs_name, birth_date
 
 #TODO Implement
 #Create a content paragraph for your landing page:
-landing_page_content = ''
+landing_page_content = "Hi there it's me Benny. Feel free to look around my page. While you're here, don't forgot to follow all my social media accounts. Thank you. "
 
 def index(request):
     response = {'name': mhs_name, 'content': landing_page_content}
