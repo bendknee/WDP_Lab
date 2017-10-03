6d23213e4842435daa6a953c60bdaad22b56785e
Benny William Pardede <bpardedewilliam@gmail.com>
2017-10-03 13:59:09 +0700

    fix settings.py bug

--- praktikum/settings.py
+++ praktikum/settings.py
@@ -49,13 +49,9 @@ INSTALLED_APPS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MIDDLEWARE = [
-    MIDDLEWARE_CLASSES = (
-        # Simplified static file serving.
-        # https://warehouse.python.org/project/whitenoise/
-        'whitenoise.middleware.WhiteNoiseMiddleware')
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
