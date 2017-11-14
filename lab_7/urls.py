0f626b43cefa949c5b4ab0e3f1ee6339aa51a5ba
Benny William Pardede <bpardedewilliam@gmail.com>
2017-11-14 16:31:13 +0700

    delete button not solved

--- lab_7/urls.py
+++ lab_7/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^add-friend/$', add_friend, name='add-friend'),
     url(r'^validate-npm/$', validate_npm, name='validate-npm'),
-    url(r'^delete-friend/(?P<friend_id>[0-9]+)/$', delete_friend, name='delete-friend'),
+    url(r'delete-friend/(?P<friend_id>[0-9]+)/$', delete_friend, name='delete-friend'),
     url(r'^friend-list/$', friend_list, name='friend-list'),
     url(r'^get-friend-list/$', get_friend_list, name='get-friend-list')
 ]
+
--- lab_7/views.py
+++ lab_7/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Friend
 from .api_csui_helper.csui_helper import CSUIhelper
@@ -16,13 +17,42 @@ def index(request):
     # Page halaman menampilkan list mahasiswa yang ada
     # TODO berikan akses token dari backend dengan menggunakaan helper yang ada
 
-    mahasiswa_list = csui_helper.instance.get_mahasiswa_list()
+    all_mahasiswa = csui_helper.instance.get_mahasiswa_list()
 
     friend_list = Friend.objects.all()
     auth = csui_helper.instance.get_auth_param_dict()
-    response = {"mahasiswa_list": mahasiswa_list, "friend_list": friend_list, "auth": auth}
+    page = request.GET.get('page', 1)
+    paginate_data = paginate_page(page, all_mahasiswa)
+    mahasiswa = paginate_data['data']
+    page_range = paginate_data['page_range']
+
+    response = {"mahasiswa_list": mahasiswa, "friend_list": friend_list, "page_range": page_range, "auth": auth}
     html = 'lab_7/lab_7.html'
     return render(request, html, response)
+
+
+def paginate_page(page, data_list):
+    paginator = Paginator(data_list, 10)
+
+    try:
+        data = paginator.page(page)
+    except PageNotAnInteger:
+        data = paginator.page(1)
+    except EmptyPage:
+        data = paginator.page(paginator.num_pages)
+
+    # Get the index of the current page
+    index = data.number - 1
+    # This value is maximum index of your pages, so the last page - 1
+    max_index = len(paginator.page_range)
+    # You want a range of 10, so lets calculate where to slice the list
+    start_index = index if index >= 10 else 0
+    end_index = 10 if index < max_index - 10 else max_index
+    # Get our new page range. In the latest versions of Django page_range returns
+    # an iterator. Thus pass it to list, to make our slice possible again.
+    page_range = list(paginator.page_range)[start_index:end_index]
+    paginate_data = {'data': data, 'page_range': page_range}
+    return paginate_data
 
 
 def friend_list(request):
@@ -44,8 +74,10 @@ def add_friend(request):
     if request.method == 'POST':
         name = request.POST['name']
         npm = request.POST['npm']
-        friend = Friend(friend_name=name, npm=npm)
-        friend.save()
+        taken = Friend.objects.filter(npm__iexact=npm).exists()
+        if not taken:
+            friend = Friend(friend_name=name, npm=npm)
+            friend.save()
         data = model_to_dict(friend)
         return HttpResponse(data)
 
